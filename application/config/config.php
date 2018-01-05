8b22b82e82a9a0fd490f3cb54b4422eb2407f562
Marcel Tauber <marceltbr@gmail.com>
2018-01-06 00:03:58 +0100

    base url https

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'play-u.herokuapp.com/';
+$config['base_url'] = 'https://play-u.herokuapp.com/';
 
 /*
 |--------------------------------------------------------------------------
